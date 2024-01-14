9a881ea30dc68a34ed668abb7db42e4409e0491e
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-14 13:49:08 +0100

    Remove unused imports and variables

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,10 @@
-import { Card } from '../../ui/dashboard/cards';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
 import { lusitana } from '../../ui/fonts';
 import { Suspense } from 'react';
 import CardWrapper from '../../ui/dashboard/cards';
 import {
-  InvoiceSkeleton,
   RevenueChartSkeleton,
-  CardSkeleton,
   InvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
